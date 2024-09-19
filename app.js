53e9ca9e9a56265522f2242e27cfbd62468db562
Harlyn01127 <harlynmaeyanez16@gmail.com>
2024-09-19 10:10:11 +0800

    z

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let loginForm = document.getElementById("userForm");
 
 loginForm.addEventListener("sumbit", (e) =>{
-e.preventDefault;
+e.preventDefault();
 
 let name = document.getElementById("name").value;
 let age = document.getElementById("age").value;
